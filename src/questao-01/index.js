fe68ea67267303cc61d568b5122bbfb729914e9b
adrianorod <adrianoazevedo.r@gmail.com>
2020-03-13 12:35:15 -0300

    feat(questao-01): termina logica e cria cli

--- src/questao-01/index.js
+++ src/questao-01/index.js
@@ -1,31 +1,76 @@
-// Dado um array de números inteiros, retorne os índices dos
-// dois números de forma que eles se somem a um alvo
-// específico.
+// config inicial
+const readline = require('readline');
 
-// Você pode assumir que cada entrada teria exatamente uma
-// solução, e você não pode usar o mesmo elemento duas
-// vezes.
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// EXEMPLO
+const question = (str) => new Promise((resolve) => rl.question(str, resolve));
 
-// Dado nums = [2, 7, 11, 15], alvo = 9,
-// Como nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
+let nums;
+let alvo;
 
-const nums = [2, 7, 11, 15];
-const alvo = 9;
-let i = 1;
-// let startIndex = 0;
+// descrição
+console.log(`Questão 01 - Dado um array de números inteiros, retornaremos os índices de dois números
+ de forma que eles se somem a um alvo específico.`);
 
-for (;;) {
-  const result = nums[startIndex] + nums[i];
+// lógica aplicada
+const runApp = () => {
+  let i = 1;
+  let startIndex = 0;
 
-  if (result === alvo) {
-    console.log([startIndex, i]);
-    break;
+  for (;;) {
+    const result = nums[startIndex] + nums[i];
+
+    if (result === alvo) {
+      console.log([startIndex, i]);
+      break;
+    }
+
+    if (i < nums.length - 1) {
+      i += 1;
+    } else if (startIndex < nums.length - 1) {
+      startIndex += 1;
+      i = startIndex + 1;
+    } else {
+      console.log('Nenhum alvo foi encontrado.');
+      break;
+    }
   }
+};
 
-  if (nums.length > i + 1) {
-    i += 1;
-  }
-}
+// perguntas para obter dados dinamicos
+const steps = {
+  start: async () => {
+    steps.array();
+  },
+  array: async () => {
+    const data = await question('Insira o array de números inteiros desejado (Ex.: [2, 7, 11, 15]): ');
+    nums = JSON.parse(data);
+
+    if (!Array.isArray(nums)) {
+      console.error(`Formato inesperado de array informado (${nums}). Tente novamente.`);
+      steps.array();
+    } else {
+      steps.alvo();
+    }
+  },
+  alvo: async () => {
+    const data = await question('Insira o alvo - deve ser um número inteiro (Ex.: 9): ');
+    alvo = JSON.parse(data);
+
+    if (!Number.isInteger(alvo)) {
+      console.error('Alvo informado não é inteiro. Tente novamente. Ex.: 9');
+      steps.alvo();
+    } else {
+      steps.end();
+    }
+  },
+  end: async () => {
+    rl.close();
+    runApp();
+  },
+};
+
+steps.start();
